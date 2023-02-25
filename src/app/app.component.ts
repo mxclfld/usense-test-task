26d1a4de9482d7b49aaf3fb9878540a969f3fbc5
crackfield <crackfield777@gmail.com>
2023-02-25 14:11:59 +0200

    Create a form with password check system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-getting-started';
+  password = '';
+  passwordStrength = '';
+
+  checkPasswordStrength() {
+    const passwordLength = this.password.length;
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasNumbers = /\d/.test(this.password);
+    const hasSymbols = /[^\w\s]/.test(this.password);
+
+    if (hasLetters && hasNumbers && hasSymbols && passwordLength >= 8) {
+      this.passwordStrength = 'strong';
+    } else if (
+      passwordLength >= 8 &&
+      ((hasLetters && !hasNumbers && hasSymbols) ||
+        (hasLetters && hasNumbers && !hasSymbols) ||
+        (!hasLetters && hasNumbers && hasSymbols))
+    ) {
+      this.passwordStrength = 'medium';
+    } else if (passwordLength < 8 || hasNumbers || hasLetters || hasSymbols) {
+      this.passwordStrength = 'weak';
+    } else {
+      this.passwordStrength = '';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, MatInputModule, MatCardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
